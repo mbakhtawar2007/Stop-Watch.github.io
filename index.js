9b601134638339401588c40c1613c80231d47b2c
Muhammad Bakhtawar <mbakhtawar.sce@gmail.com>
2024-03-31 12:39:16 +0500

    Completed 100%

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
-function start(){
+let [seconds, minutes, hours] = [0, 0 ,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
 
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+
+    displayTime.innerHTML = h +":"+ m +":"+ s;
 }
+
+function start(){
+    if(timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000)
+}
+
+let [seconds, minutes, hours] = [0, 0 ,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+
+    displayTime.innerHTML = h +":"+ m +":"+ s;
+}
+
+function start(){
+    if(timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000)
+}
+
+
+
 function stop(){
-    
+    clearInterval(timer);
 }
+
 function reset(){
-    
+ clearInterval(timer);
+ [seconds, minutes, hours] = [0, 0 ,0];
+ displayTime.innerHTML = "00:00:00";
+  
+}
+
+function stop(){
+    clearInterval(timer);
+}
+
+function reset(){
+ clearInterval(timer);
+ [seconds, minutes, hours] = [0, 0 ,0];
+ displayTime.innerHTML = "00:00:00";
+  
 }
